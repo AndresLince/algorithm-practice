3eec5e01be8b0322b8e932e8f056fd821f38fb59
AndresLince <lynxdarmatron@gmail.com>
2022-09-28 05:32:40 -0500

    chapter 1 test small refactor

--- src/chapter1.test.ts
+++ src/chapter1.test.ts
@@ -1,19 +1,20 @@
 import { Chapter1 } from './chapter1'
 
 describe('Chapter1', () => {
+    let chapter1 = new Chapter1();
     test('should return true for hasAllUniqueCharacters("algo")', () => {
-        expect(new Chapter1().hasAllUniqueCharacters('algo')).toBe(true)
+        expect(chapter1.hasAllUniqueCharacters('algo')).toBe(true)
     })
     test('should return false for hasAllUniqueCharacters("algoa")', () => {
-        expect(new Chapter1().hasAllUniqueCharacters('algoa')).toBe(false)
+        expect(chapter1.hasAllUniqueCharacters('algoa')).toBe(false)
     })
     test('should return true for checkPermutation("algo","loga")', () => {
-        expect(new Chapter1().checkPermutation('algo','loga')).toBe(true)
+        expect(chapter1.checkPermutation('algo','loga')).toBe(true)
     })
     test('should return false for checkPermutation("algoa","algo")', () => {
-        expect(new Chapter1().checkPermutation('algoa', 'algo')).toBe(false)
+        expect(chapter1.checkPermutation('algoa', 'algo')).toBe(false)
     })
     test('should return false for checkPermutation("aabbcc","aaabcc")', () => {
-        expect(new Chapter1().checkPermutation('aabbcc', 'aaabcc')).toBe(false)
+        expect(chapter1.checkPermutation('aabbcc', 'aaabcc')).toBe(false)
     })
 })
