0d199c0b2c963a9e51cb8077942cbd5b05a67474
AndresLince <lynxdarmatron@gmail.com>
2022-10-28 17:31:06 -0500

    Add Eloquent javascript chapter 3 exercise 2

--- src/eloquentjavascript.ts
+++ src/eloquentjavascript.ts
@@ -200,4 +200,37 @@ export class EloquentJavascriptExercises {
         })
         return sum;
     }
+    /**
+        Reversing an array
+        Arrays have a reverse method that changes the array by inverting the
+        order in which its elements appear. For this exercise, write two
+        functions, reverseArray and reverseArrayInPlace. The first,
+        reverseArray, takes an array as argument and produces a new array
+        that has the same elements in the inverse order. The second,
+        reverseArrayInPlace, does what the reverse method does: it modifies the
+        array given as argument by reversing its elements. Neither may use the
+        standard reverse method.
+        Thinking back to the notes about side effects and pure functions in the
+        previous chapter, which variant do you expect to be useful in more
+        situations? Which one runs faster?
+     */
+    reverseArray(array: Array<any>): Array<any> {
+        let newArray: Array<any> = []
+        for (let i = array.length - 1; i >= 0; i--) {
+            newArray.push(array[i]);
+        }
+        return newArray;
+    }
+    reverseArrayInPlace(array: Array<any>): Array<any> {
+        let startIndex = 0;
+        let endIndex = array.length - 1;
+        while (startIndex != endIndex) {
+            let startValue = array[startIndex];
+            array[startIndex] = array[endIndex];
+            array[endIndex] = startValue;
+            startIndex++;
+            endIndex--;
+        }
+        return array;
+    }
 }
