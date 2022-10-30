0f59c29e3e4f692ac90081da6d84b95d666a630c
AndresLince <lynxdarmatron@gmail.com>
2022-10-30 18:11:24 -0500

    Add Eloquent javascript chapter 3 exercise 4

--- src/eloquentjavascript.ts
+++ src/eloquentjavascript.ts
@@ -282,4 +282,48 @@ export class EloquentJavascriptExercises {
         let newArray = linkedList.toArray(linkedList);
         return newArray;
     }
+    /**
+        The == operator compares objects by identity. But sometimes you’d
+        prefer to compare the values of their actual properties.
+        Write a function deepEqual that takes two values and returns true only
+        if they are the same value or are objects with the same properties,
+        where the values of the properties are equal when compared with a
+        recursive call to deepEqual.
+
+        To find out whether values should be compared directly
+        (use the === operator for that) or have their properties compared, you
+        can use the typeof operator. If it produces "object" for both values,
+        you should do a deep comparison. But you have to take one silly
+        exception into account: because of a historical accident, typeof null
+        also produces "object".
+
+        The Object.keys function will be useful when you need to go over the
+        properties of objects to compare them.
+     */
+    deepEqual(object1: any, object2: any): boolean {
+        if (object1 === null && object2 === null) {
+            return true;
+        }
+        if (object1 === null || object2 === null) {
+            return false;
+        }
+        if (typeof object1 != "object" || typeof object2 != "object") {
+            return object1 === object2;
+        }
+
+        let keysObject1 = Object.keys(object1);
+        let keysObject2 = Object.keys(object2);
+
+        if (keysObject1.length != keysObject2.length) {
+            return false;
+        }
+
+        for (let i = 0; i < keysObject1.length; i++) {
+            let response = this.deepEqual(object1[keysObject1[i]], object2[keysObject1[i]]);
+            if (response === false) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
