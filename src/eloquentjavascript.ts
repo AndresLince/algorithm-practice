da3ecc8af0abd0ec7b40e4daa23dbfbbdf14a6bb
AndresLince <lynxdarmatron@gmail.com>
2022-11-03 18:13:20 -0500

    dd Eloquent javascript chapter 5 exercise 4

--- src/eloquentjavascript.ts
+++ src/eloquentjavascript.ts
@@ -1,4 +1,5 @@
 import { Node } from "./LinkedList";
+import { SCRIPTS } from "./scripts";
 
 export class EloquentJavascriptExercises {
     /**
@@ -381,5 +382,59 @@ export class EloquentJavascriptExercises {
     everyWithSome(array: Array<number>, test): boolean {
         let response = array.some(element => !test(element));
         return !response;
+    }
+    /**
+        Dominant writing direction
+        Write a function that computes the dominant writing direction in a
+        string of text. Remember that each script object has a direction
+        property that can be "ltr" (left to right), "rtl" (right to left), or
+        "ttb" (top to bottom).
+        The dominant direction is the direction of a majority of the characters
+        that have a script associated with them. The characterScript and
+        countBy functions defined earlier in the chapter are probably useful
+        here.
+     */
+    characterScript(code) {
+        for (let script of SCRIPTS) {
+            if (script.ranges.some(([from, to]) => {
+                return code >= from && code < to;
+            })) {
+                return script;
+            }
+        }
+        return null;
+    }
+    countBy(items, groupName) {
+        let counts = [];
+        for (let item of items) {
+            let name = groupName(item);
+            let known = counts.findIndex(c => c.name == name);
+            if (known == -1) {
+                counts.push({ name, count: 1 });
+            } else {
+                counts[known].count++;
+            }
+        }
+        return counts;
+    }
+    dominantDirection(text) {
+        let scripts = this.countBy(text, char => {
+            let script = this.characterScript(char.codePointAt(0));
+            return script ? script.direction : "none";
+        }).filter(({ name }) => name != "none");
+        const mostRepitedDirection = scripts.reduce(
+            (previousValue: any, currentValue: any) => {
+                if (previousValue.count) {
+                    if (previousValue.count > currentValue.count) {
+                        return previousValue;
+                    }
+                    return currentValue;
+                } else {
+                    return currentValue;
+                }
+            },
+            0
+        );
+        return mostRepitedDirection.name;
     }
 }
