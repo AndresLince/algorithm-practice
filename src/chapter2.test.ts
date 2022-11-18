19c77d48e1cc1496879ad5123c3664e221ca3d20
AndresLince <lynxdarmatron@gmail.com>
2022-11-18 06:12:04 -0500

    Cracking the coding interview exercise 2 chapter 2 tests Linked Lists

--- src/chapter2.test.ts
+++ src/chapter2.test.ts
@@ -1,8 +1,10 @@
 import { Chapter2 } from "./chapter2";
 import { Node } from "./LinkedList";
+import { EloquentJavascriptExercises } from './eloquentjavascript';
 
 describe('Chapter2', () => {
-    let chapter1 = new Chapter2();
+    let chapter2 = new Chapter2();
+    let eloquentjavascriptExercises = new EloquentJavascriptExercises();
     test('should remove the duplicated nodes of a linked list removeDups(head)', () => {
         let expectedResult = new Node(1);
         expectedResult.appendToTail(2);
@@ -18,6 +20,15 @@ describe('Chapter2', () => {
         node.appendToTail(2);
         node.appendToTail(9);
         node.appendToTail(7);
-        expect(chapter1.removeDups(node)).toStrictEqual(expectedResult);
+        expect(chapter2.removeDups(node)).toStrictEqual(expectedResult);
+    })
+    test('should return the 2 to last node from a linked list kthToLast(head, 2)', () => {
+        let linkedList = eloquentjavascriptExercises.arrayToList([1, 2, 3, 4, 5, 6, 7]);
+        let expectedResult = eloquentjavascriptExercises.arrayToList([5, 6, 7]);
+        expect(chapter2.kthToLast(linkedList, 2)).toStrictEqual(expectedResult)
+    })
+    test('should return the 5 to last node from a linked list kthToLast(head, 5)', () => {
+        let linkedList = eloquentjavascriptExercises.arrayToList([1, 2, 3]);
+        expect(chapter2.kthToLast(linkedList, 5)).toBe(null)
     })
 });
