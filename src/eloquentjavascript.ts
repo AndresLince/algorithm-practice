a34a80655191bbd72a0818386ceb862aac44b924
AndresLince <lynxdarmatron@gmail.com>
2022-11-27 14:53:23 -0500

    Add Eloquent javascript chapter 6 exercise 4

--- src/eloquentjavascript.ts
+++ src/eloquentjavascript.ts
@@ -437,4 +437,19 @@ export class EloquentJavascriptExercises {
         );
         return mostRepitedDirection.name;
     }
+    /**
+     * Borrowing a method
+     * Earlier in the chapter I mentioned that an object's hasOwnProperty can
+     * be used as a more robust alternative to the in operator when you want to
+     * ignore the prototype's properties. But what if your map needs to include
+     * the word "hasOwnProperty"? You won't be able to call that method anymore
+     * because the object's own property hides the method value.
+     * Can you think of a way to call hasOwnProperty on an object that has its
+     * own property by that name?
+     */
+    borrowingMethod(object: Object, property: string): boolean{
+        const toStringSymbol = Symbol("hasOwnProperty");
+        object[toStringSymbol] = true;
+        return object.hasOwnProperty(property)
+    }
 }
