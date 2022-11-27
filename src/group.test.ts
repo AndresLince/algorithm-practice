38cd25b05e8b347d9f7a6ea70d053e3105fb8b05
AndresLince <lynxdarmatron@gmail.com>
2022-11-27 14:07:04 -0500

    Add Eloquent javascript chapter 6 exercise 3 tests

--- src/group.test.ts
+++ src/group.test.ts
@@ -1,9 +1,8 @@
 import { Group } from "./group"
 
 describe('Group class', () => {
-    let groupClass = new Group();
     test('Should create a group', () => {
-        let group = groupClass.from([1, 2 ,3, 4, 3, 2 , 1])
+        let group = Group.from([1, 2 ,3, 4, 3, 2 , 1])
         expect(group.group).toStrictEqual([1, 2, 3, 4]);
     })
     test('Should add an item to the group property', () => {
@@ -12,21 +11,28 @@ describe('Group class', () => {
         expect(group.group).toStrictEqual([1]);
     })
     test('Shouldnt add an item to the group property', () => {
-        let group = groupClass.from([1, 2, 3, 4, 5])
+        let group = Group.from([1, 2, 3, 4, 5])
         group.add(1);
         expect(group.group).toStrictEqual([1, 2, 3, 4, 5]);
     })
     test('Should remove an item from the group property', () => {
-        let group = groupClass.from([1, 2, 3]);
+        let group = Group.from([1, 2, 3]);
         group.remove(2);
         expect(group.group).toStrictEqual([1, 3]);
     })
     test('should return true for has(5)', () => {
-        let group = groupClass.from([1, 2, 3, 4, 5]);
+        let group = Group.from([1, 2, 3, 4, 5]);
         expect(group.has(5)).toBe(true);
     })
     test('should return false for has(5)', () => {
-        let group = groupClass.from([1, 2, 3, 4]);
+        let group = Group.from([1, 2, 3, 4]);
         expect(group.has(5)).toBe(false);
+    })
+    test('should iterate correctly', () => {
+        let array = [];
+        for (let value of Group.from(["a", "b", "c"])) {
+            array.push(value)
+        }
+        expect(array).toStrictEqual(["a", "b", "c"]);
     })
 })
