ebd3245746bae0d31f32e601bf0920fb1e68f33e
AndresLince <lynxdarmatron@gmail.com>
2022-12-04 11:30:42 -0500

    Add Eloquent javascript chapter 7 exercise 1

--- src/EloquentJavascriptChapter7.ts
+++ src/EloquentJavascriptChapter7.ts
@@ -118,4 +118,28 @@ export class EJC7 {
         }
         return { direction: route[0], memory: route.slice(1) };
     }
+    /**
+     * Measuring a robot
+     * It's hard to objectively compare robots by just letting them solve a few
+     * scenarios. Maybe one robot just happened to get easier tasks or the kind of
+     * tasks that it is good at, whereas the other didn't.
+     * Write a function compareRobots that takes two robots (and their starting
+     * memory). It should generate 100 tasks and let each of the robots solve each
+     * of these tasks. When done, it should output the average number of steps each
+     * robot took per task.
+     * For the sake of fairness, make sure you give each task to both robots,
+     * rather than generating different tasks per robot.
+    */
+    static compareRobots(robot1, memory1, robot2, memory2) {
+        let totalTurns1 = 0;
+        let totalTurns2 = 0;
+        for (let i = 0; i < 100; i++) {
+            let task = VillageState.random();
+            totalTurns1 += EJC7.runRobot(task, robot1, memory1);
+            totalTurns2 += EJC7.runRobot(task, robot2, memory2);
+        }
+        let average1 = totalTurns1 / 100;
+        let average2 = totalTurns2 / 100;
+        return { average1, average2 }
+    }
 }
