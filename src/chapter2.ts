b0ca9423613fb6a6e1daa3a8282461849b94fb7e
AndresLince <lynxdarmatron@gmail.com>
2022-12-13 07:29:48 -0500

    Cracking the coding interview exercise 7 chapter 2 Linked Lists

--- src/chapter2.ts
+++ src/chapter2.ts
@@ -165,4 +165,27 @@ export class Chapter2 {
         }
         return true;
     }
+    /**
+     * 2.7
+     * Intersection: Given two (singly) linked lists, determine if the two
+     * lists intersect. Return the intersecting node. Note that the
+     * intersection is defined based on reference, not value. That is, if the
+     * kth node of the first linked list is the exact same node (by reference)
+     * as the jth node of the second linked list, then they are intersecting.
+     */
+    isIntersection(node1: Node, node2: Node): Node {
+        let pointer: Node;
+        let len2 = node2.len();
+        while (node1 != null) {
+            pointer = node2;
+            for (let i = 0; i < len2; i++) {
+                if (node1 === pointer) {
+                    return pointer;
+                }
+                pointer = pointer.next;
+            }
+            node1 = node1.next;
+        }
+        return null;
+    }
 }
