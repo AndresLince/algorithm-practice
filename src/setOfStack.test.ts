96b401d24a820b84608595e9281eb0ea76eafa19
AndresLince <lynxdarmatron@gmail.com>
2023-01-04 07:54:44 -0500

    Cracking the coding interview chapter 3 exercise 3 follow up tests Stacks and Queues

--- src/setOfStack.test.ts
+++ src/setOfStack.test.ts
@@ -42,4 +42,48 @@ describe('SetOfStack', () => {
         stack.push(8);
         expect(stack.peak()).toBe(8);
     })
+    test('SetOfStack should return a valid value for popAt()', () => {
+        const stack = new SetOfStacks(3);
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        stack.push(6);
+        stack.push(7);
+        stack.push(8);
+        stack.push(9);
+        stack.popAt(1);
+        stack.popAt(1);
+        stack.popAt(1);
+        stack.pop()
+        stack.pop()
+        stack.pop()
+        stack.pop()
+        stack.pop()
+        expect(stack.peak()).toBe(1);
+    })
+    test('SetOfStack should return a valid value for popAt()', () => {
+        const stack = new SetOfStacks(3);
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        stack.push(6);
+        stack.push(7);
+        stack.push(8);
+        stack.push(9);
+        stack.popAt(1);
+        stack.popAt(1);
+        stack.popAt(1);
+        expect(function () { stack.popAt(1) }).toThrow("EmptyStackException");
+    })
+    test('SetOfStack should return a valid value for popAt()', () => {
+        const stack = new SetOfStacks(3);
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        expect(function () { stack.popAt(1) }).toThrow("EmptyStackException");
+    })
 })
