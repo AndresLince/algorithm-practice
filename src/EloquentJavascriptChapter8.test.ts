58ed3dfcc9a2ab5e68f199765fb3ad935f15d4d1
AndresLince <lynxdarmatron@gmail.com>
2023-01-22 21:19:48 -0500

    Add Eloquent javascript chapter 8 exercise 2 tests

--- src/EloquentJavascriptChapter8.test.ts
+++ src/EloquentJavascriptChapter8.test.ts
@@ -1,9 +1,25 @@
 import { EJC8 } from "./EloquentJavascriptChapter8"
 
 describe('EloquentJavascriptChapter8', () => {
+    const ejc8 = new EJC8();
     test('should return a valid value for reliableMultiply ', () => {
-        const ejc8 = new EJC8()
         const result = ejc8.reliableMultiply(8,8);
         expect(result).toBe(64);
+    })
+    test('should return a valid value for withBoxUnlocked() ', () => {
+        ejc8.withBoxUnlocked((box) => {
+            box.content.push("gold piece");
+        });
+        expect(function () { ejc8.box.content }).toThrow("Locked!");
+        ejc8.box.unlock();
+        expect(ejc8.box._content.length).toBe(1);
+        expect(ejc8.box._content[0]).toBe("gold piece");
+    })
+    test('should return a valid value for withBoxUnlocked() ', () => {
+        expect(function () {
+            ejc8.withBoxUnlocked(() => {
+                throw new Error("Pirates on the horizon! Abort!");
+            })
+        }).toThrow("Pirates on the horizon! Abort!");
     })
 })
