ae4a0c40aefcc01cc968a94c33720965598fd2ad
AndresLince <lynxdarmatron@gmail.com>
2023-01-24 20:57:57 -0500

    Add Eloquent javascript chapter 9 exercise 1 tests WIP

---
+++ src/EloquentJavascriptChapter9.test.ts
@@ -0,0 +1,21 @@
+import { EJC9 } from "./EloquentJavascriptChapter9"
+
+describe('EloquentJavascriptChapter9', () => {
+    const ejc9 = new EJC9();
+    test('should return true for codeGolf(1)', () => {
+        const validation = ejc9.codeGolf(1);
+        expect(validation).toBe(true);
+    })
+    test('should return false for codeGolf(8)', () => {
+        const validation = ejc9.codeGolf(8);
+        expect(validation).toBe(false);
+    })
+    test('should return false for codeGolf(9)', () => {
+        const validation = ejc9.codeGolf(9);
+        expect(validation).toBe(false);
+    })
+    test('should return false for codeGolf(10)', () => {
+        const validation = ejc9.codeGolf(10);
+        expect(validation).toBe(false);
+    })
+})
