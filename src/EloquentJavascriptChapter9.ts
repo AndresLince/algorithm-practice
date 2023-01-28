a4783b138eadac2e173718f0e8fdb921bf8357bd
AndresLince <lynxdarmatron@gmail.com>
2023-01-28 14:31:04 -0500

    Add Eloquent javascript chapter 9 exercise 3

--- src/EloquentJavascriptChapter9.ts
+++ src/EloquentJavascriptChapter9.ts
@@ -87,4 +87,35 @@ export class EJC9 {
     quotingStyle(text: string): string {
         return text.replace(/(^|\W)'|'(\W|$)/g, '$1"$2');
     }
+    /**
+     * Numbers again
+     * Write an expression that matches only JavaScript-style numbers.It must
+     * support an optional minus or plus sign in front of the number, the
+     * decimal dot, and exponent notation—5e-3 or 1E10—again with an optional
+     * sign in front of the exponent.Also note that it is not necessary for
+     * there to be digits in front of or after the dot, but the number cannot
+     * be a dot alone.That is, .5 and 5. are valid JavaScript numbers, but a
+     * lone dot isn't.
+     */
+    numbersAgain(): boolean {
+        let number = /^[+\-]?(\d+(\.\d*)?|\.\d+)([eE][+\-]?\d+)?$/;
+        const validNumbers = ["1", "-1", "+15", "1.55", ".5", "5.", "1.3e2", "1E-4", "1e+12"];
+        const invalidNumbers = ["1a", "+-1", "1.2.3", "1+1", "1e4.5",".5.", "1f5", "."];
+
+        for (let str of validNumbers) {
+            if (!number.test(str)) {
+                console.log(`Failed to match '${str}'`);
+                return false;
+            }
+        }
+
+        for (let str of invalidNumbers) {
+            if (number.test(str)) {
+                console.log(`Incorrectly accepted '${str}'`);
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
