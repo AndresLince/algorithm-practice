41df290db00dd579ac466fc2e4dd9a834ec1bb48
AndresLince <lynxdarmatron@gmail.com>
2023-01-28 14:30:17 -0500

    Add Eloquent javascript chapter 9 exercise 2 tests improvement

--- src/EloquentJavascriptChapter9.test.ts
+++ src/EloquentJavascriptChapter9.test.ts
@@ -42,7 +42,7 @@ describe('EloquentJavascriptChapter9', () => {
         const validation = ejc9.codeGolf(7);
         expect(validation).toBe(true);
     })
-    test('should return true for quotingStyle(text)', () => {
+    test('should return a valid string for quotingStyle(text)', () => {
         const text = "'I'm the cook,' he said, 'it's my job.'";
         const result = ejc9.quotingStyle(text);
         const expectedText = "\"I'm the cook,\" he said, \"it's my job.\""
